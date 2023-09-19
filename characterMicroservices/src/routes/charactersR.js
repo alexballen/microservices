03967dda2b6f01992cccf1cbfa69bd3bb9c9cbad
Alex <alexballen53@gmail.com>
2023-09-18 19:09:31 -0500

    Se le pasa a la ruta para que realice la validacion del personaje con el middleware antes de ejecutar el controlador

--- characterMicroservices/src/routes/charactersR.js
+++ characterMicroservices/src/routes/charactersR.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const controller = require("../controller/index.js");
+const middlewares = require("../middlewares");
 
 const router = Router();
 
 router.get("/", controller.getCharacter);
+router.post("/", middlewares.characterValidation, controller.createCharacter);
 
 module.exports = router;
