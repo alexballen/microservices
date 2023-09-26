c2006fc4fd65fa322f3b30e6079c1237ea339b8b
Alex <alexballen53@gmail.com>
2023-09-25 20:11:43 -0500

    Se añade ruta asincrona para pedir de la db la info de los personajes

--- characterMicroservices/src/data/index.js
+++ characterMicroservices/src/data/index.js
@@ -1,8 +1,11 @@
 const characters = require("../data/characters.json");
+const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    return characters;
+    const result = await axios.get("http://localhost:8004/Character");
+    console.log(result);
+    return result;
   },
   create: async () => {
     throw Error("Hay un error en la DB al momento de crear el personaje");
