3173d5cdaec0e0ef9c7a731776343adae805edba
Alex <alexballen53@gmail.com>
2023-09-26 20:46:23 -0500

    Se crean nuevas rutas para ejecutar los diferentes controladores

--- characterMicroservices/src/routes/charactersR.js
+++ characterMicroservices/src/routes/charactersR.js
@@ -4,7 +4,10 @@ const middlewares = require("../middlewares");
 
 const router = Router();
 
-router.get("/", controller.getCharacter);
+router.get("/", controller.getCharacters);
+router.get("/:id", controller.getCharacter);
 router.post("/", middlewares.characterValidation, controller.createCharacter);
+router.put("/:id", controller.updateCharacter);
+router.delete("/:id", controller.deleteCharacter);
 
 module.exports = router;
