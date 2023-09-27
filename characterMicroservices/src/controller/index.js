0322c2303ec0bff15655fdbf9403e2ef115e7065
Alex <alexballen53@gmail.com>
2023-09-26 20:43:37 -0500

    Se añaden al objeto que almacena los valores de los controladores nuevos controladores

--- characterMicroservices/src/controller/index.js
+++ characterMicroservices/src/controller/index.js
@@ -1,6 +1,9 @@
 const { catchedAsync } = require("../utils");
 
 module.exports = {
+  getCharacters: catchedAsync(require("./getCharacters.js")),
   getCharacter: catchedAsync(require("./getCharacter.js")),
   createCharacter: catchedAsync(require("./createCharacter.js")),
+  updateCharacter: catchedAsync(require("./updateCharacter.js")),
+  deleteCharacter: catchedAsync(require("./deleteCharacter.js")),
 };
