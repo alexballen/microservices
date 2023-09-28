b7923c1cc1e59724de00bc5a6436c36efde065cd
Alex <alexballen53@gmail.com>
2023-09-27 19:36:10 -0500

    Se añaden los middlewares validadores a las rutas, para que se realice la validacion antes de llamar al controlador

--- characterMicroservices/src/routes/charactersR.js
+++ characterMicroservices/src/routes/charactersR.js
@@ -1,13 +1,23 @@
 const { Router } = require("express");
 const controller = require("../controller/index.js");
-const middlewares = require("../middlewares");
+const middlewares = require("../middlewares/index.js");
 
 const router = Router();
 
 router.get("/", controller.getCharacters);
-router.get("/:id", controller.getCharacter);
-router.post("/", middlewares.characterValidation, controller.createCharacter);
-router.put("/:id", controller.updateCharacter);
-router.delete("/:id", controller.deleteCharacter);
+router.get("/:id", middlewares.validateId, controller.getCharacter);
+
+router.post("/", middlewares.validateObject, controller.createCharacter);
+
+router.put("/", middlewares.validateId);
+router.put(
+  "/:id",
+  middlewares.validateId,
+  middlewares.validateObject,
+  controller.updateCharacter
+);
+
+router.delete("/", middlewares.validateId);
+router.delete("/:id", middlewares.validateId, controller.deleteCharacter);
 
 module.exports = router;
