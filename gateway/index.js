6e9ab6b2a34545b02d15534a9b6378cda6d059e7
Alex <alexballen53@gmail.com>
2023-09-27 19:26:20 -0500

    Se elimina ruta proxy para el microservicio database

--- gateway/index.js
+++ gateway/index.js
@@ -30,14 +30,6 @@ server.use(
   })
 );
 
-server.use(
-  "/database",
-  createProxyMiddleware({
-    target: "http://database:8004",
-    changeOrigin: true,
-  })
-);
-
 server.listen(8000, () => {
   console.log("Gateway on port 8000");
 });
