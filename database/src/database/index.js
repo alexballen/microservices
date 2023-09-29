fff05c206f7469b027b295dd25ed46839e6899fd
Alex <alexballen53@gmail.com>
2023-09-28 20:07:50 -0500

    Se elimina codigo comentado inservible

--- database/src/database/index.js
+++ database/src/database/index.js
@@ -3,15 +3,6 @@ const { MONGO_URI } = require("../config/envs");
 
 const conn = mongoose.createConnection(MONGO_URI);
 
-/* const Character = conn.model("Character", require("./schemas/characterSchema"));
-const Film = conn.model("Film", require("./schemas/filmSchema"));
-const Planet = conn.model("Planet", require("./schemas/planetSchema"));
-
-Character.find()
-  .populate("homeworld", ["_id", "name"])
-  .populate("films", ["_id", "title"])
-  .then((res) => console.log(res[3])); */
-
 module.exports = {
   Character: conn.model("Character", require("./schemas/characterSchema")),
   Film: conn.model("Film", require("./schemas/filmSchema")),
