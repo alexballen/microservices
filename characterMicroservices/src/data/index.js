2134cbb4350ae9683e5cdb1bef577044f2e8ca1d
Alex <alexballen53@gmail.com>
2023-09-28 19:43:12 -0500

    Se modifica la ruta de la solicitud http de localhost al nombre definido en el proxy database

--- characterMicroservices/src/data/index.js
+++ characterMicroservices/src/data/index.js
@@ -2,32 +2,36 @@ const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    const getCharacter = await axios.get("http://localhost:8004/Character");
+    const getCharacter = await axios.get("http://database:8004/Character");
     return getCharacter.data;
   },
+
   getById: async (id) => {
     const getCharacterById = await axios.get(
-      `http://localhost:8004/Character/${id}`
+      `http://database:8004/Character/${id}`
     );
     return getCharacterById.data;
   },
+
   create: async (character) => {
     const createCharacter = await axios.post(
-      "http://localhost:8004/Character",
+      "http://database:8004/Character",
       character
     );
     return createCharacter.data;
   },
+
   update: async (id, updatedData) => {
     const updatedFields = await axios.put(
-      `http://localhost:8004/Character/${id}`,
+      `http://database:8004/Character/${id}`,
       updatedData
     );
     return updatedFields.data;
   },
+
   delete: async (id) => {
     const deleteCharacter = await axios.delete(
-      `http://localhost:8004/Character/${id}`
+      `http://database:8004/Character/${id}`
     );
     return deleteCharacter.data;
   },
