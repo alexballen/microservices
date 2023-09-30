fa1c859134d5169005872f5f5d4f31a343f4c8f9
Alex <alexballen53@gmail.com>
2023-09-30 12:01:24 -0500

    Configurar controladores y envolverlos en funciones asincrónicas 'catchedAsync' para gestionar errores de manera consistente en las rutas de películas

--- filmsMicroservices/src/controller/index.js
+++ filmsMicroservices/src/controller/index.js
@@ -1,6 +1,9 @@
-const { catchedAsync } = require("../utils/index.js");
+const { catchedAsync } = require("../utils");
 
 module.exports = {
   getFilms: catchedAsync(require("./getFilms.js")),
+  getFilm: catchedAsync(require("./getFilm.js")),
   createFilm: catchedAsync(require("./createFilm.js")),
+  updateFilm: catchedAsync(require("./updateFilm.js")),
+  deleteFilm: catchedAsync(require("./deleteFilm.js")),
 };
