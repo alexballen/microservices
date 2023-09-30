140fcbe09a557f51b1c64cbb589d0781230bd6aa
Alex <alexballen53@gmail.com>
2023-09-30 11:59:47 -0500

    Implementar controlador para obtener una película por su ID y responder con los detalles de la película

---
+++ filmsMicroservices/src/controller/getFilm.js
@@ -0,0 +1,8 @@
+const Film = require("../data");
+const { response } = require("../utils");
+
+module.exports = async (req, res) => {
+  const { id } = req.params;
+  const film = await Film.getById(id);
+  response(res, 201, film);
+};
