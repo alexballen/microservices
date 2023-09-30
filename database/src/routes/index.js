c3ed134108732f8c21cf6cbed73871606db797a3
Alex <alexballen53@gmail.com>
2023-09-29 20:50:58 -0500

    Se elminan las rutas y se redirige al componente que las crea para modularizar

--- database/src/routes/index.js
+++ database/src/routes/index.js
@@ -1,39 +1,8 @@
 const { Router } = require("express");
-const store = require("../database");
-const { validateModel } = require("../middlewares");
+const databaseR = require("./databaseR");
 
 const router = Router();
 
-router.get("/:model", validateModel, async (req, res) => {
-  const { model, id } = req.params;
-  const response = await store[model].list();
-  res.status(200).json(response);
-});
-
-router.get("/:model/:id", validateModel, async (req, res) => {
-  const { model, id } = req.params;
-  const response = await store[model].get(id);
-  res.status(200).json(response);
-});
-
-router.post("/:model", validateModel, async (req, res) => {
-  const { model } = req.params;
-  const character = req.body;
-  const response = await store[model].insert(character);
-  res.status(200).json(response);
-});
-
-router.put("/:model/:id", validateModel, async (req, res) => {
-  const { model, id } = req.params;
-  const updatedData = req.body;
-  const response = await store[model].update(id, updatedData);
-  res.status(200).json(response);
-});
-
-router.delete("/:model/:id", validateModel, async (req, res) => {
-  const { model, id } = req.params;
-  const response = await store[model].delete(id);
-  res.status(200).json(response);
-});
+router.use("/", databaseR);
 
 module.exports = router;
