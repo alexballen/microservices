f818412e70188ca903180a7007323f1456abba68
Alex <alexballen53@gmail.com>
2023-09-30 12:02:29 -0500

    Configurar cliente Axios para realizar solicitudes HTTP a un servicio de películas en el servidor de base de datos

--- filmsMicroservices/src/data/index.js
+++ filmsMicroservices/src/data/index.js
@@ -1,10 +1,31 @@
-const films = require("../data/films.json");
+const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    return films;
+    const getFilm = await axios.get("http://database:8004/Film");
+    return getFilm.data;
   },
-  create: async () => {
-    throw Error("Hay un error en la DB al momento de crear la pelicula");
+
+  getById: async (id) => {
+    const getFilmById = await axios.get(`http://database:8004/Film/${id}`);
+    return getFilmById.data;
+  },
+
+  create: async (film) => {
+    const createFilm = await axios.post("http://database:8004/Film", film);
+    return createFilm.data;
+  },
+
+  update: async (id, updatedData) => {
+    const updatedFields = await axios.put(
+      `http://database:8004/Film/${id}`,
+      updatedData
+    );
+    return updatedFields.data;
+  },
+
+  delete: async (id) => {
+    const deleteFilm = await axios.delete(`http://database:8004/Film/${id}`);
+    return deleteFilm.data;
   },
 };
