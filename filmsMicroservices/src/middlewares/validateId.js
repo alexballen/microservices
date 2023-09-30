6d514987630043ddfe82d1afd61962ab63c352d6
Alex <alexballen53@gmail.com>
2023-09-30 12:04:46 -0500

    Implementar middleware de validación de ID de película para asegurarse de que el ID sea un número válido

---
+++ filmsMicroservices/src/middlewares/validateId.js
@@ -0,0 +1,14 @@
+const { ClientError } = require("../utils/errors");
+
+module.exports = (req, res, next) => {
+  const { id } = req.params;
+  if (id) {
+    if (/^\d+$/.test(id)) {
+      next(); // Si contiene solo dígitos, continúa
+    } else {
+      throw new ClientError("ID no es un número válido¡", 401);
+    }
+  } else {
+    throw new ClientError("Falta el id de la pelicula¡", 401);
+  }
+};
