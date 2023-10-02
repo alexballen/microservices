eb1786bb4b18a4e558d9a1fc85e64ebe9726e43a
Alex <alexballen53@gmail.com>
2023-10-01 19:39:02 -0500

    Configurar controladores y envolverlos en funciones asincrónicas 'catchedAsync' para gestionar errores de manera consistente en las rutas de planetas

--- planetsMicroservices/src/controller/index.js
+++ planetsMicroservices/src/controller/index.js
@@ -1,6 +1,9 @@
-const { catchedAsync } = require("../utils/index.js");
+const { catchedAsync } = require("../utils");
 
 module.exports = {
   getPlanets: catchedAsync(require("./getPlanets.js")),
+  getPlanet: catchedAsync(require("./getPlanet.js")),
   createPlanet: catchedAsync(require("./createPlanet.js")),
+  updatePlanet: catchedAsync(require("./updatePlanet.js")),
+  deletePlanet: catchedAsync(require("./deletePlanet.js")),
 };
