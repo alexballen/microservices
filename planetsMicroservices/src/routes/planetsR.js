e3abb031fb7dea7aab52322239d304b7804edf05
Alex <alexballen53@gmail.com>
2023-10-02 21:56:02 -0500

    Configurar rutas y middleware para gestionar operaciones CRUD en planetas

--- planetsMicroservices/src/routes/planetsR.js
+++ planetsMicroservices/src/routes/planetsR.js
@@ -1,10 +1,23 @@
 const { Router } = require("express");
-const controller = require("../controller/index.js");
+const controller = require("../controller");
 const middlewares = require("../middlewares");
 
 const router = Router();
 
 router.get("/", controller.getPlanets);
-router.post("/", middlewares.planetsValidation, controller.createPlanet);
+router.get("/:id", middlewares.validateId, controller.getPlanet);
+
+router.post("/", middlewares.validateObject, controller.createPlanet);
+
+router.put("/", middlewares.validateId);
+router.put(
+  "/:id",
+  middlewares.validateId,
+  middlewares.validateObject,
+  controller.updatePlanet
+);
+
+router.delete("/", middlewares.validateId);
+router.delete("/:id", middlewares.validateId, controller.deletePlanet);
 
 module.exports = router;
