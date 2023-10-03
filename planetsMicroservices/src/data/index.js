1a250de57b3d50a6575b829dd0742d0e923b6c8b
Alex <alexballen53@gmail.com>
2023-10-02 21:52:57 -0500

    Configurar cliente Axios para realizar solicitudes HTTP a un servicio de planetas en el servidor de base de datos

--- planetsMicroservices/src/data/index.js
+++ planetsMicroservices/src/data/index.js
@@ -1,10 +1,36 @@
-const planets = require("../data/planets.json");
+const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    return planets;
+    const getPlanet = await axios.get("http://database:8004/Planet");
+    return getPlanet.data;
   },
-  create: async () => {
-    throw Error("Hay un error en la DB al momento de crear el planeta");
+
+  getById: async (id) => {
+    const getPlanetById = await axios.get(`http://database:8004/Planet/${id}`);
+    return getPlanetById.data;
+  },
+
+  create: async (planet) => {
+    const createPlanet = await axios.post(
+      "http://database:8004/Planet",
+      planet
+    );
+    return createPlanet.data;
+  },
+
+  update: async (id, updatedData) => {
+    const updatedFields = await axios.put(
+      `http://database:8004/Planet/${id}`,
+      updatedData
+    );
+    return updatedFields.data;
+  },
+
+  delete: async (id) => {
+    const deletePlanet = await axios.delete(
+      `http://database:8004/Planet/${id}`
+    );
+    return deletePlanet.data;
   },
 };
