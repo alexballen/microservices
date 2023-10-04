f2d3ca8c6283780a5c302d7b99d0be6a66941bf8
Alex <alexballen53@gmail.com>
2023-10-04 17:31:26 -0500

    Configurar rutas y middleware para gestionar operaciones CRUD en películas

--- filmsMicroservices/src/routes/filmsR.js
+++ filmsMicroservices/src/routes/filmsR.js
@@ -5,19 +5,35 @@ const middlewares = require("../middlewares");
 const router = Router();
 
 router.get("/", controller.getFilms);
-router.get("/:id", middlewares.validateId, controller.getFilm);
+router.get(
+  "/:id",
+  middlewares.validateId,
+  middlewares.searchById,
+  controller.getFilm
+);
 
-router.post("/", middlewares.validateObject, controller.createFilm);
+router.post(
+  "/",
+  middlewares.findDuplicateId,
+  middlewares.validateObject,
+  controller.createFilm
+);
 
 router.put("/", middlewares.validateId);
 router.put(
   "/:id",
   middlewares.validateId,
+  middlewares.searchById,
   middlewares.validateObject,
   controller.updateFilm
 );
 
 router.delete("/", middlewares.validateId);
-router.delete("/:id", middlewares.validateId, controller.deleteFilm);
+router.delete(
+  "/:id",
+  middlewares.validateId,
+  middlewares.searchById,
+  controller.deleteFilm
+);
 
 module.exports = router;
