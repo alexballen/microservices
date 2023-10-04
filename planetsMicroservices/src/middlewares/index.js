d95d7ea2d65646aec323188fcc8e4d8a6e7c85b4
Alex <alexballen53@gmail.com>
2023-10-04 17:32:57 -0500

    Exportar módulos de validación y búsqueda desde archivos respectivos

--- planetsMicroservices/src/middlewares/index.js
+++ planetsMicroservices/src/middlewares/index.js
@@ -1,4 +1,6 @@
 module.exports = {
   validateId: require("./validateId.js"),
   validateObject: require("./validateObject.js"),
+  searchById: require("./searchById.js"),
+  findDuplicateId: require("./findDuplicateId.js"),
 };
